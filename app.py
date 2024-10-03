ed6c76c3eee9fd1f251b3efb7f713230be76be6d
Duarte Chen <duartechen@gmail.com>
2024-10-03 11:09:06 +0100

    Flask server with web interface working. Only adds horses and lists them.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_mysqldb import MySQL
 from datetime import datetime
@@ -26,23 +27,23 @@ def allowed_file(filename):
 @app.route('/horses')
 def get_horses():
     cursor = mysql.connection.cursor()
-    cursor.execute('''SELECT * FROM horses''')
+
+    cursor.execute('SELECT h.id, h.name, h.weight, h.health_score, hp.picture_name FROM horses h LEFT JOIN horse_pictures hp ON h.id = hp.horse_id')
     horses = cursor.fetchall()
     cursor.close()
     
     return render_template('horses.html', horses=horses)
 
 
-
 @app.route('/add_horse', methods=['GET', 'POST'])
 def add_horse():
     if request.method == 'POST':
-        # Get form data
+        #os campos do formulário
         name = request.form['name']
         weight = request.form['weight']
         health_score = request.form['health_score']
-        """
-        # Handle picture upload
+        
+
         if 'picture' not in request.files:
             return 'No file part'
         
@@ -52,28 +53,24 @@ def add_horse():
             filename = secure_filename(file.filename)
             picture_name = filename
             date_taken = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            
-            # Save the file to the uploads folder
+        
+
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             
+            
+
             cursor = mysql.connection.cursor()
 
-
+            cursor.execute('INSERT INTO horses (name, weight, health_score) VALUES (%s, %s, %s)', (name, weight, health_score)) 
             
-            # Insert the picture information into the 'horse_pictures' table
-            cursor.execute('''INSERT INTO horse_pictures (horse_id, picture_name, date_taken) 
-                              VALUES (%s, %s, %s)''', (horse_id, picture_name, date_taken))
-"""
-            # Insert horse data into the 'horses' table
-        cursor.execute('''INSERT INTO horses (name, weight, health_score) 
-                            VALUES (%s, %s, %s)''', (name, weight, health_score))
-        horse_id = cursor.lastrowid  # Get the ID of the newly inserted horse            
+            horse_id = cursor.lastrowid         
+            
 
-        # Commit the changes and close the cursor
+            cursor.execute('INSERT INTO horse_pictures (horse_id, picture_name, date_taken) VALUES (%s, %s, %s)', (horse_id, picture_name, date_taken))
+
         mysql.connection.commit()
         cursor.close()
 
-        # Redirect to the list of horses
         return redirect(url_for('get_horses'))
 
     return render_template('add_horse.html')
